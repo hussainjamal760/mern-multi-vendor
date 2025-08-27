dd0bd360a991d79909aa4b0c95725db68e40e21e
Hussain Jamal <Hjamal9865@gmail.com>
2025-08-27 19:59:35 +0500

    backend setup done

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+import express from "express"
+import dotenv from "dotenv"
+const app =express()
+
+if(process.env.NODE_ENV !== "PRODUCTION"){
+    dotenv.config({
+        path:"backend/config/.env"
+    })
+}
+
+export default app
