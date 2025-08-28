87d775b9f6d0566301daf7d813a4921f31c95778
Hussain Jamal <Hjamal9865@gmail.com>
2025-08-28 11:47:14 +0500

    user model created

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
+import ErrorHandler from "./utils/ErrorHandler.js"
+import cookieParser from "cookie-parser"
+import fileUpload from "express-fileupload"
 const app =express()
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(fileUpload({useTempFiles:true}))
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
     dotenv.config({
         path:"backend/config/.env"
     })
 }
+
+app.use(ErrorHandler)
 
 export default app
