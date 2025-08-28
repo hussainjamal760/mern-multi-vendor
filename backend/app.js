447ca260c448409cb3137a4a52d1aad88c0b10cb
Hussain Jamal <Hjamal9865@gmail.com>
2025-08-28 11:53:35 +0500

    multer added

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 import ErrorHandler from "./utils/ErrorHandler.js"
 import cookieParser from "cookie-parser"
-import fileUpload from "express-fileupload"
 const app =express()
 
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(fileUpload({useTempFiles:true}))
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
     dotenv.config({
